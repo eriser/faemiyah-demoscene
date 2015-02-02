f49fe2a676005c1ca58d60aa0c2caeef0d879c48
trilkk <trilkk@9f8bc12f-273e-2d68-7472-eab2f63d9766>
2015-02-02 23:40:17 +0000

    Reintegrate changes from local branch. Release.

--- trunk/dnload/src/dnload.h
+++ trunk/dnload/src/dnload.h
@@ -86,19 +86,6 @@ extern "C" {
 #endif
 
 #if !defined(USE_LD)
-#if defined(__FreeBSD__)
-#if defined(__clang__)
-/** Symbol required by libc. */
-void *environ;
-/** Symbol required by libc. */
-void *__progname;
-#else
-/** Symbol required by libc. */
-void *environ __attribute__((externally_visible));
-/** Symbol required by libc. */
-void *__progname __attribute__((externally_visible));
-#endif
-#endif
 #if defined(__clang__)
 /** Program entry point. */
 void _start();
@@ -167,20 +154,20 @@ static struct SymbolTableStruct
   void (GLAPIENTRY *glProgramUniform3fv)(GLuint, GLint, GLsizei, const GLfloat*);
 } g_symbol_table =
 {
-  (int (*)(int))0xb88bf697L,
-  (void (*)(void))0xda43e6eaL,
-  (GLuint (GLAPIENTRY *)(GLenum, GLsizei, const char**))0xa4fd03d8L,
-  (int (*)(SDL_Event*))0x64949d97L,
-  (void (GLAPIENTRY *)(GLsizei, GLuint*))0x75e35418L,
-  (int (*)(Uint32))0x70d6574L,
-  (void (*)(int))0x29f14a4L,
-  (void (GLAPIENTRY *)(GLuint))0x2386bc04L,
-  (void (*)(void))0x7eb657f3L,
-  (void (GLAPIENTRY *)(GLuint, GLbitfield, GLuint))0x212d8ad7L,
-  (void (GLAPIENTRY *)(GLshort, GLshort, GLshort, GLshort))0xd419e20aL,
-  (int (*)(SDL_AudioSpec*, SDL_AudioSpec*))0x46fd70c8L,
-  (SDL_Surface* (*)(int, int, int, Uint32))0x39b85060L,
-  (void (GLAPIENTRY *)(GLuint, GLint, GLsizei, const GLfloat*))0xc969d24eL,
+  (int (*)(int))0xb88bf697,
+  (void (*)(void))0xda43e6ea,
+  (GLuint (GLAPIENTRY *)(GLenum, GLsizei, const char**))0xa4fd03d8,
+  (int (*)(SDL_Event*))0x64949d97,
+  (void (GLAPIENTRY *)(GLsizei, GLuint*))0x75e35418,
+  (int (*)(Uint32))0x70d6574,
+  (void (*)(int))0x29f14a4,
+  (void (GLAPIENTRY *)(GLuint))0x2386bc04,
+  (void (*)(void))0x7eb657f3,
+  (void (GLAPIENTRY *)(GLuint, GLbitfield, GLuint))0x212d8ad7,
+  (void (GLAPIENTRY *)(GLshort, GLshort, GLshort, GLshort))0xd419e20a,
+  (int (*)(SDL_AudioSpec*, SDL_AudioSpec*))0x46fd70c8,
+  (SDL_Surface* (*)(int, int, int, Uint32))0x39b85060,
+  (void (GLAPIENTRY *)(GLuint, GLint, GLsizei, const GLfloat*))0xc969d24e,
 };
 #endif
 
@@ -239,7 +226,7 @@ typedef Elf32_Sym dnload_elf_sym_t;
 typedef Elf32_Sword dnload_elf_tag_t;
 #endif
 /** \brief ELF base address. */
-#define ELF_BASE_ADDRESS 0x2000000
+#define ELF_BASE_ADDRESS 0x400000
 /** \brief Get the address associated with given tag in a dynamic section.
  *
  * \param dyn Dynamic section.
